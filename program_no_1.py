8e04ee5e57a3e59ee024b4ee42300274ce83bbf9
mithunadrenaline <119931991+mithunadrenaline@users.noreply.github.com>
2022-12-07 22:23:24 +0530

    Delete program_no_1.py

--- program_no_1.py
+++
@@ -1,8 +0,0 @@
-name=input("Enter your name : ")
-location=input("Enter your location : ")
-containment_zones=["Kappil","Odayam", "Manthara","Sreeyettu","Vettakada","Attingal","Kazhakootam","Sreekaryam"]
-
-if(location==containment_zones):
-  print("Stay safe.....You are in Containment zone")
-else:
-  print("You are in safe zone.")
