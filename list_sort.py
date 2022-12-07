2ce5f6a97cb15c1f111f8dc1bdff6fdaefc9633f
mithunadrenaline <119931991+mithunadrenaline@users.noreply.github.com>
2022-12-07 22:41:51 +0530

    Update list_sort.py

--- list_sort.py
+++ list_sort.py
@@ -3,5 +3,6 @@ n=int(input("Enter the limit : \t"))
 for i in range(0,n+1):
   a=int(input())
   myList.append(a)
+print("List is : ",myList)
 myList.sort()
-print(myList)
+print("Sorted list is : ",myList)
